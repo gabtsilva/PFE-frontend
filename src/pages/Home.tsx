6c456c80f714faef1d5ffac8983fbc5fdefd3795
Gabriel Torres <gabriel.torres.silva@hotmail.com>
2023-12-05 14:39:02 +0100

    Working frontend to backend connexion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import Header from "../components/Header";
+import {useEffect, useState} from "react";
 
 const Home: React.FC = () => {
-  return (
+    const [data, setData] = useState(null);
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/hello');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+                console.log("not ok");
+            }
+            console.log(response);
+            const result = await response.json();
+            setData(result);
+        } catch (error) {
+            console.log("error " + error);
+            console.error('Error fetching data:', error);
+        }
+    };
+    fetchData();
+
+    return (
     <IonPage>
         <Header />
       <IonContent fullscreen>
+          <div>
+              {JSON.stringify(data)}
+          </div>
       </IonContent>
     </IonPage>
   );
