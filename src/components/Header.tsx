d0125828bdc6ce8cde98d8a34abcb36c42b91191
juliendec <juliendecoen2308@gmail.com>
2023-12-07 12:14:12 +0100

    menu sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,107 @@
-import {IonHeader, IonImg, IonToolbar} from "@ionic/react";
-import logo from '../../assets/img/logo_snappies.png';
-import './Header.css';
+import React from "react";
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonImg,
+  IonRouterLink,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+} from "@ionic/react";
+import logo from "../../assets/img/logo_snappies.png";
+import "./Header.css";
+import {
+  accessibilityOutline,
+  homeOutline,
+  clipboardOutline,
+  cubeOutline,
+  busOutline,
+  walkOutline,
+  footstepsOutline,
+} from "ionicons/icons";
 
-interface HeaderProps { }
+interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
-    return (
+  return (
+    <>
+      <IonMenu contentId="main-content">
         <IonHeader>
-            <IonToolbar>
-                <IonImg class="ion-margin" alt="Snappies' logo" src={logo}></IonImg>
-            </IonToolbar>
+          <IonToolbar>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
         </IonHeader>
-    );
+        <IonContent className="ion-padding">
+          <IonList>
+            <IonRouterLink routerLink="/">
+              <IonItem>
+                <IonIcon icon={homeOutline}></IonIcon>
+                <IonLabel>Home</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/clients">
+              <IonItem>
+                <IonIcon icon={accessibilityOutline}></IonIcon>
+                <IonLabel>Clients</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/articles">
+              <IonItem>
+                <IonIcon icon={clipboardOutline}></IonIcon>
+                <IonLabel>Articles</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/commandes">
+              <IonItem>
+                <IonIcon icon={cubeOutline}></IonIcon>
+                <IonLabel>Commandes</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/vehicules">
+              <IonItem>
+                <IonIcon icon={busOutline}></IonIcon>
+                <IonLabel>Véhicules</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/tournees">
+              <IonItem>
+                <IonIcon icon={walkOutline}></IonIcon>
+                <IonLabel>Tournées</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+            <IonRouterLink routerLink="/livreurs">
+              <IonItem>
+                <IonIcon icon={footstepsOutline}></IonIcon>
+                <IonLabel>Livreurs</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonImg
+              class="ion-margin"
+              alt="Snappies' logo"
+              src={logo}
+              slot="end"
+            ></IonImg>
+          </IonToolbar>
+        </IonHeader>
+      </IonPage>
+    </>
+  );
 };
 
 export default Header;
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,19 +1,74 @@
-import './LoginForm.css';
-import {IonButton, IonImg, IonInput, IonItem} from "@ionic/react";
+import "./LoginForm.css";
+import { IonButton, IonImg, IonInput, IonItem } from "@ionic/react";
 import logo from "../../assets/img/logo_snappies.png";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const handleSubmit = async () => {
+    // Construire le corps de la requête
+    const requestBody = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      // Envoyer la requête POST
+      const response = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      // Vérifier la réponse
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.token);
+        // La requête a réussi, vous pouvez traiter la réponse ici
+        console.log("Connexion réussie !");
+
+        // Stocker le token dans le localStorage
+        localStorage.setItem("token", data.token);
+
+        // Rediriger vers la page d'accueil
+        history.push("/");
+      } else {
+        // La requête a échoué, vous pouvez gérer les erreurs ici
+        console.error("Échec de la connexion");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la connexion", error);
+    }
+  };
+
   return (
-      <div id="container">
-          <IonImg alt="Snappies' logo" src={logo}></IonImg>
-          <IonItem lines="none">
-              <IonInput type="email" placeholder="Email"></IonInput>
-          </IonItem>
-          <IonItem lines="none">
-              <IonInput type="password" placeholder="Mot de passe"></IonInput>
-          </IonItem>
-          <IonButton color="light" fill="outline">Se connecter</IonButton>
-      </div>
+    <div id="container">
+      <IonImg alt="Snappies' logo" src={logo}></IonImg>
+      <IonItem lines="none">
+        <IonInput
+          type="email"
+          placeholder="Email"
+          onIonChange={(e) => setEmail(e.detail.value!)}
+        ></IonInput>
+      </IonItem>
+      <IonItem lines="none">
+        <IonInput
+          type="password"
+          placeholder="Mot de passe"
+          onIonChange={(e) => setPassword(e.detail.value!)}
+        ></IonInput>
+      </IonItem>
+      <IonButton color="light" fill="outline" onClick={handleSubmit}>
+        Se connecter
+      </IonButton>
+    </div>
   );
 };
 
